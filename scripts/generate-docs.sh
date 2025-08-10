8908dce966a060c318976a340631a60f9ed72cc8
Prashant V <prashant@prashantv.com>
2025-08-09 17:25:38 -0700

    Run tests for all modules and packages (#4)

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -4,7 +4,7 @@ set -x
 
 # regenerate _site with docs
 rm -rf _site
-doc2go -home go.prashantv.com $(go work edit  -json | jq -r '.Use[].DiskPath | . + "/..."')
+doc2go -home go.prashantv.com $(mise run list-module-packages)
 
 # add meta tag for go import
 find _site -iname *.html -exec go run scripts/appendmeta/appendmeta.go {} \;
